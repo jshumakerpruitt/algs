ddd6f659a295cfb399e34a0a312742b854bba28f
Jasper S <jshumakerpruitt@gmail.com>
2014-12-28 20:48:45 -0800

    cleanup digraph, add #reverse

--- digraph.rb
+++ digraph.rb
@@ -1,14 +1,33 @@
 class Digraph
-  def initialize
-    @edges = Hash.new { |h, k| h[k] = [] }
+  def initialize(edges = Hash.new { |h, k| h[k] = [] })
+    @edges = edges 
   end
     
   def add_edge(v,w)
     @edges[v] << w
   end
   
+  def num_edges
+    @edges.inject(0) {|acc, pair| acc + pair[1].count }
+  end
+
+  def num_verts
+    @edges.count
+  end
+
   def adj(v)
     @edges[v]
   end
 
+  def reverse
+    reversed = Digraph.new
+    @edges.each do |v, bag_of_edges|
+      bag_of_edges.each do |edge|
+        reversed.add_edge(edge, v)
+      end
+    end
+    reversed
+  end
+      
 end
+
