63ac664f32940cb8c3c6cf3800a6c2a6ac02685e
Jasper S <jshumakerpruitt@gmail.com>
2014-12-28 22:07:54 -0800

    messy cycle detection for digraph

--- digraph.rb
+++ digraph.rb
@@ -1,6 +1,11 @@
 class Digraph
+  attr_accessor :edges
   def initialize(edges = Hash.new { |h, k| h[k] = [] })
     @edges = edges 
+    @cycle = [] 
+    @edge_to = Hash.new { |h, k| h[k] = nil }
+    @marked = Hash.new { |h, k| h[k] = nil }
+    @on_stack = []
   end
     
   def add_edge(v,w)
@@ -28,6 +33,41 @@ class Digraph
     end
     reversed
   end
-      
+  
+  def has_cycle?
+    return true if @cycle.count > 1
+    false
+  end
+
+  
+  def dfs(v)
+    #all credit due to Bob Sedgwick
+    #stores cycle
+    @on_stack[v] = true
+    @marked[v] = true
+    p v
+    @edges[v].each do |w|
+      puts 'looping'
+      if has_cycle?
+        puts 'has cycle'
+        return
+      elsif !@marked[w]
+        puts 'recurse'
+        @edge_to[w] = v
+        dfs(w)
+      elsif @on_stack[w] 
+        puts 'cycle found'
+        x = v
+        until x == w
+          @cycle << x
+          x = @edge_to[x]
+          puts 'building cycle'
+        end
+        @cycle << w
+        @cycle << v 
+      end
+    end
+    @on_stack[v] = false
+  end
 end
 
